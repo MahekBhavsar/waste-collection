aef0a866cf3e05c7274551a7b37732326694de92
MahekBhavsar <mahek.bhavsar29@gmail.com>
2026-02-06 21:52:41 +0530

    Add PostcodeForm component

--- src/components/PostcodeForm.js
+++ src/components/PostcodeForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react";  
 
 export default function PostcodeForm({ onSearch }) {
   const [postcode, setPostcode] = useState("");
